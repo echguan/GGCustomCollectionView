cb01c88cb852d6bc1f310c438457c0eafe2af140
echgg <echguan@sina.com>
2015-05-02 02:05:11 +0800

    collection view controller

---
+++ GGCustomCollectionView/CustomClass/GGCustomCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  GGCustomCollectionViewController.h
+//  GGCustomCollectionView
+//
+//  Created by echgg on 15/5/2.
+//  Copyright (c) 2015年 echgg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGCustomCollectionViewController : UICollectionViewController<UICollectionViewDataSource, UICollectionViewDelegate>
+
+@end
