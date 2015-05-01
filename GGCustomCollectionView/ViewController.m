e7f63d1df56b6f25c1a6ebb2ab9b30c5a7af5231
echguan <echgg@huangyuuandeAir.lan>
2015-05-02 01:34:31 +0800

    Initial Commit

---
+++ GGCustomCollectionView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GGCustomCollectionView
+//
+//  Created by echgg on 15/5/2.
+//  Copyright (c) 2015年 echgg. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
